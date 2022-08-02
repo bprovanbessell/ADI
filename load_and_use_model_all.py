027875fc818a466dda7ba2a5a0ef88f86af4f4fd
Benjamin Provan-Bessell <benjaminlpb123@gmail.com>
2022-08-02 09:40:03 +0100

    Fix error time

--- load_and_use_model_all.py
+++ load_and_use_model_all.py
@@ -139,7 +139,7 @@ err_time_end = time.mktime(time.strptime("20.10.2021 21:00:00", "%d.%m.%Y %H:%M:
 
 # Not really cleare when the error ended, as the motor followed a different pattern of operation.
 err_time_start = time.mktime(time.strptime("09.02.2021 12:00:00", "%d.%m.%Y %H:%M:%S"))
-err_time_end = time.mktime(time.strptime("09.02.2021 16:20:00", "%d.%m.%Y %H:%M:%S"))
+err_time_end = time.mktime(time.strptime("09.02.2021 16:00:00", "%d.%m.%Y %H:%M:%S"))
 
 
 
